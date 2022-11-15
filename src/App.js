5e8022906f8831702a95e840368ea99b93816d2d
unknown <avni.mittal2002@gmail.com>
2022-11-15 18:09:52 +0530

    React app setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Body from "./components/body";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body></Body>
+      Team Deimos
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/body.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Body extends Component {
+    state = {  }
+    render() {
+        return ( 
+            <h1>Body</h1>
+         );
+    }
+}
+
+export default Body
